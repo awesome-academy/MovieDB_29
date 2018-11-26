08d7050893a66e3c4695d423ac23bde2cd87bffc
Vo Huu Ly <vhlee1996@gmail.com>
2018-11-26 15:33:10 +0700

    Fetch data categories screen

--- app/src/main/java/com/framgia/vhlee/themoviedb/service/RetrofitBuilder.java
+++ app/src/main/java/com/framgia/vhlee/themoviedb/service/RetrofitBuilder.java
@@ -1,15 +1,22 @@
 package com.framgia.vhlee.themoviedb.service;
 
+import com.framgia.vhlee.themoviedb.BuildConfig;
 import com.framgia.vhlee.themoviedb.utils.Constants;
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitBuilder {
+    private static final String API_KEY = "api_key";
     private static final int READ_TIMEOUT = 5000;
     private static final int WRITE_TIMEOUT = 5000;
     private static final int CONNECT_TIMEOUT = 5000;
@@ -21,6 +28,21 @@ public class RetrofitBuilder {
                 .writeTimeout(WRITE_TIMEOUT, TimeUnit.MILLISECONDS)
                 .connectTimeout(CONNECT_TIMEOUT, TimeUnit.MILLISECONDS)
                 .retryOnConnectionFailure(true);
+        httpClientBuilder.addInterceptor(new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request request = chain.request();
+                HttpUrl httpUrl = request
+                        .url()
+                        .newBuilder()
+                        .addQueryParameter(API_KEY, BuildConfig.API_KEY)
+                        .build();
+                Request.Builder requestBuilder = request
+                        .newBuilder()
+                        .url(httpUrl);
+                return chain.proceed(requestBuilder.build());
+            }
+        });
         if (sRetrofit == null) {
             sRetrofit = new Retrofit.Builder()
                     .baseUrl(Constants.BASE_URL)
--- app/src/main/java/com/framgia/vhlee/themoviedb/ui/category/CategoryViewModel.java
+++ app/src/main/java/com/framgia/vhlee/themoviedb/ui/category/CategoryViewModel.java
@@ -4,24 +4,82 @@ import android.databinding.BaseObservable;
 import android.databinding.ObservableArrayList;
 
 import com.framgia.vhlee.themoviedb.data.model.Movie;
+import com.framgia.vhlee.themoviedb.data.model.MovieResponse;
 import com.framgia.vhlee.themoviedb.data.repository.MovieRepository;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class CategoryViewModel extends BaseObservable {
     private static final String TAG = "CategoryViewModel";
     private static final int DEFAULT_PAGE = 1;
+    private int mPage;
     private String mType;
     private ObservableArrayList<Movie> mMovies;
     private MovieRepository mMovieRepository;
+    private CompositeDisposable mCompositeDisposable;
 
-    public CategoryViewModel() {
+    public CategoryViewModel(String type) {
+        mPage = DEFAULT_PAGE;
+        mType = type;
         mMovies = new ObservableArrayList<>();
         mMovieRepository = MovieRepository.getInstance();
+        mCompositeDisposable = new CompositeDisposable();
     }
 
-    public void loaMovies() {
+    public void loaMovies(boolean isGenre) {
+        if (isGenre) loaGenreMovies();
+        else loadCategoryMovies();
     }
 
     public ObservableArrayList<Movie> getMovies() {
         return mMovies;
+    }
+
+    public void destroy() {
+        mCompositeDisposable.dispose();
+    }
+
+    private void loaGenreMovies() {
+        Disposable disposable = mMovieRepository.getMoviesByGenre(mType, mPage)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<MovieResponse>() {
+                    @Override
+                    public void accept(MovieResponse movieResponse) throws Exception {
+                        mMovies.addAll(movieResponse.getResults());
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        handleError(throwable.getMessage());
+                    }
+                });
+        mCompositeDisposable.add(disposable);
+    }
+
+    private void loadCategoryMovies() {
+        Disposable disposable = mMovieRepository.getMoviesByCategory(mType, mPage)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<MovieResponse>() {
+                    @Override
+                    public void accept(MovieResponse movieResponse) throws Exception {
+                        mMovies.addAll(movieResponse.getResults());
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        handleError(throwable.getMessage());
+                    }
+                });
+        mCompositeDisposable.add(disposable);
+    }
+
+    private void handleError(String message) {
+        //TODO handle error
     }
 }
